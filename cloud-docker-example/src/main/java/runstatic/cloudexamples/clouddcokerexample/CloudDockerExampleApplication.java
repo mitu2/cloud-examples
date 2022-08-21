9cbba727d0e148f3538c648a6f4899c02020cb30
chenmoand <chenmoand@outlook.com>
2022-08-21 16:11:46 +0800

    add cloud-docker-example

---
+++ cloud-docker-example/src/main/java/runstatic/cloudexamples/clouddcokerexample/CloudDockerExampleApplication.java
@@ -0,0 +1,16 @@
+package runstatic.cloudexamples.clouddcokerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenmoand
+ */
+@SpringBootApplication
+public class CloudDockerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDockerExampleApplication.class, args);
+    }
+
+}
