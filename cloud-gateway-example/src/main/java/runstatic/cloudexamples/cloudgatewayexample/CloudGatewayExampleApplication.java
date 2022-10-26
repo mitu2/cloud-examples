3fe19a91e3c9a8547925fe8cba62b47b065c8523
chenmoand <chenmoand@outlook.com>
2022-10-26 20:23:40 +0800

    add sentinel-dashboard.jar

--- cloud-gateway-example/src/main/java/runstatic/cloudexamples/cloudgatewayexample/CloudGatewayExampleApplication.java
+++ cloud-gateway-example/src/main/java/runstatic/cloudexamples/cloudgatewayexample/CloudGatewayExampleApplication.java
@@ -1,5 +1,6 @@
 package runstatic.cloudexamples.cloudgatewayexample;
 
+import org.apache.curator.framework.recipes.cache.NodeCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
